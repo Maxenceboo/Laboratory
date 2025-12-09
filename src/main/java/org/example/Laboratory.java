87e8dcab1d73b2fe1c1ddb1d8cff97a03b307b9b
Maxenceboo <maxenceboo@gmail.com>
2025-12-09 10:47:40 +0100

    [REFACTOR] Store known substances internally

--- src/main/java/org/example/Laboratory.java
+++ src/main/java/org/example/Laboratory.java
@@ -1,9 +1,12 @@
 package org.example;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Laboratory {
+    private final List<String> knownSubstances;
 
     public Laboratory(List<String> substances) {
+        this.knownSubstances = new ArrayList<>(substances);
     }
 }
